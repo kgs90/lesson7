3586db1b85feee9e5e99896f1c35bb629b0c6b1f
kgs90 <kgs90@inbox.ru>
2022-06-14 18:04:41 +0300

    zadacha1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,17 @@
+﻿double[,] array = new double[1000,1000];
+int i = 0, j = 0;
+
+Random random = new Random();
+Console.WriteLine("Задайте двумерный массив MxN через пробел: ");
+int[] size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+for (i = 0; i < size[1]; i++)
+    for (j = 0; j < size[0]; j++)
+        array[i,j] = random.NextDouble();
+
+for (i = 0; i < size[1]; i++) 
+{
+    for (j = 0; j < size[0]; j++)
+        Console.Write(Math.Round(array[i,j], 2) + " ");
+    Console.WriteLine();
+}
